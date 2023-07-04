b37a7f4e8ac8efc7b9655e4ddaad9b90a5ddfcf0
badahertz52 <badahertz52@gmail.com>
2023-07-04 19:17:36 +0900

    [Feat, Style]  Add functions to PlayList, PlayListItem components  and Modify style.module.scss in PlayList

--- src/components/PlayList/PlayListItem.tsx
+++ src/components/PlayList/PlayListItem.tsx
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classNames from "classnames";
 import styles from "./style.module.scss";
-import { MusicListItemType } from "../../store/types";
-const cx = classNames.bind(styles);
+import { MusicListItemType, MusicPlayerState } from "../../store/types";
+import { shallowEqual, useSelector } from "react-redux";
 
 type PlayListItemProps = {
   item: MusicListItemType;
   index: number;
 };
 function PlayListItem({ item, index }: PlayListItemProps) {
+  const currentIndex = useSelector(
+    (state: MusicPlayerState) => state.currentIndex
+  );
+  const [duration, setDuration] = useState<string>("00:00");
+
+  const getDuration = (src: string) => {
+    return new Promise<string>((resolve) => {
+      const audio = new Audio();
+      audio.onloadedmetadata = () => {
+        const min = `${~~(audio.duration / 60)}`;
+        const sec = `${~~(audio.duration % 60)}`;
+        const minutes: string = min.length >= 2 ? min : 0 + min;
+        const seconds: string = sec.length >= 2 ? sec : 0 + sec;
+        resolve(`${minutes}:${seconds}`);
+      };
+      audio.src = src;
+    });
+  };
+
+  useEffect(() => {
+    async function getTimer() {
+      const duration = await getDuration(item.src);
+      setDuration(duration);
+    }
+    getTimer();
+  }, [item.src]);
   return (
     <>
-      <div className={cx("row")}>
-        <span>{item.name}</span>
-        <p>{item.artist}</p>
+      <div
+        className={classNames(styles.row, {
+          [styles.playing]: currentIndex === index,
+        })}
+      >
+        <span aria-description="song name">{item.name}</span>
+        <p aria-description="artist">{item.artist}</p>
       </div>
-      <span className={cx("music-duration")}>00:00</span>
+      <span
+        className={classNames(styles.musicDuration, {
+          [styles.playing]: currentIndex === index,
+        })}
+      >
+        {duration}
+      </span>
     </>
   );
 }
--- src/components/PlayList/index.tsx
+++ src/components/PlayList/index.tsx
@@ -1,11 +1,21 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useCallback } from "react";
 import QueueMusic from "@mui/icons-material/QueueMusic";
 import Close from "@mui/icons-material/Close";
 import PlayListItem from "./PlayListItem";
-import classNames from "classnames";
-import { initialPlayList } from "../../store/data";
 import styles from "./style.module.scss";
-const cx = classNames.bind(styles);
+import { initialPlayList } from "../../store/data";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  MusicListItemType,
+  MusicListType,
+  MusicPlayerState,
+} from "../../store/types";
+import {
+  setCurrentIndex,
+  updatePlayList,
+} from "../../store/musicPlayerReducer";
+import classNames from "classnames";
+import { SortableList } from "@badahertz52/sortable-list-tsc";
 
 type PlayListProps = {
   showPlayList: boolean;
@@ -13,22 +23,54 @@ type PlayListProps = {
 };
 
 const PlayList = ({ showPlayList, setShowPlayList }: PlayListProps) => {
+  const playList = useSelector((state: MusicPlayerState) => state.playList);
+  const dispatch = useDispatch();
+  const onClickClosePlayList = useCallback(() => {
+    setShowPlayList(false);
+  }, [setShowPlayList]);
+  const onClickItem = useCallback(
+    (index: number) => {
+      dispatch(setCurrentIndex(index));
+    },
+    [dispatch]
+  );
+
+  const updateData = useCallback(
+    (newPlayList: MusicListType) => {
+      dispatch(updatePlayList(newPlayList));
+    },
+    [dispatch]
+  );
+
+  const renderItem = useCallback(
+    (item: MusicListItemType, index: number) => (
+      <PlayListItem item={item} index={index} />
+    ),
+    []
+  );
+
   return (
-    <div className={cx("play-list")}>
+    <div
+      className={classNames(styles.playList, {
+        [styles.playListShow]: showPlayList,
+      })}
+    >
       <div className={styles.header}>
         <div className={styles.row}>
           <QueueMusic className={styles.list} />
           <span>Play list</span>
         </div>
-        <Close sx={{ fontSize: 22, cursor: "pointer" }} />
+        <Close
+          sx={{ fontSize: 22, cursor: "pointer" }}
+          onClick={onClickClosePlayList}
+        />
       </div>
-      <ul>
-        {initialPlayList.map((item, index) => (
-          <li key={index}>
-            <PlayListItem item={item} index={index} />
-          </li>
-        ))}
-      </ul>
+      <SortableList
+        data={playList}
+        onClickItem={onClickItem}
+        renderItem={renderItem}
+        updateData={updateData}
+      />
     </div>
   );
 };
