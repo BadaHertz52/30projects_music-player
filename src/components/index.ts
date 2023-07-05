57ba5c22536e2083363733623f33d8c50420c513
badahertz52 <badahertz52@gmail.com>
2023-07-05 16:26:59 +0900

    Make PlayIco

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,15 +2,16 @@ import Controls from "./Controls";
 import MusicPlayer from "./MusicPlayer";
 import PlayList from "./PlayList";
 import PlayListItem from "./PlayList/PlayListItem";
+import PlayIcon from "./PlayIcon";
 import ProgressArea from "./ProgressArea";
 import RepeatBtn from "./RepeatBtn";
 import SongDetail from "./SongDetail";
-
 export {
   Controls,
   MusicPlayer,
   PlayList,
   PlayListItem,
+  PlayIcon,
   ProgressArea,
   RepeatBtn,
   SongDetail,
